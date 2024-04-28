99afa18b216b730b0145a62020b3b170d42eead6
nikhildhage <nikhil.dhage@gmail.com>
2024-04-28 00:02:07 -0700

    Updated database file

---
+++ Model/database.php
@@ -0,0 +1,19 @@
+<?php
+
+// Database connection settings
+
+$dsn = "mysql:host=localhost;dbname=vehicles_admin";
+$db_user = 'root';
+$db_pass = ' ';
+
+try {
+    // Create PDO connection
+    $db = new PDO($dsn, $db_user);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Catch any errors and display
+    $error_message = "Database Error ";
+    $error_message .= " " . $e->getMessage();
+    echo $error_message;
+    exit();
+}
