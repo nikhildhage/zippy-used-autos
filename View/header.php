87e50662512cbcf6ed3550e53988d09e4a40d5a7
nikhildhage <nikhil.dhage@gmail.com>
2024-05-01 18:35:19 -0700

    Updated header file

---
+++ View/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zippy-Used-Autos Assignment</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./View/css/style.css">
+</head>
+
+<body class="flex flex-col justify-center items-center bg-slate-100 h-screen">
+    <header class="w-full bg-black text-white px-10 py-10">
+        <h5 class="font-bold text-center text-nowrap text-5xl">Zippy's Used Autos</h5>
+    </header>
+    <div id="app-container" class="container w-full flex flex-col  bg-slate-100 mx-4 my-4 px-4 py-4">
