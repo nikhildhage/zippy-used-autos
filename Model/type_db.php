6e876a699fbc04690293b0fe4a700a2f337c88ca
nikhildhage <nikhil.dhage@gmail.com>
2024-05-01 20:59:43 -0700

    Working on get_type method in type_db file

---
+++ Model/type_db.php
@@ -0,0 +1,11 @@
+<?php
+function get_types()
+{
+    global $db;
+    $query = "SELECT * from type t";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return $results;
+}
