6f63fa9f130d0ad36e558d9777f62c3f7ad5c9cf
nikhildhage <nikhil.dhage@gmail.com>
2024-05-07 19:52:12 -0500

    Added input filters in index.php

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ require_once('./Model/type_db.php');
 require_once('./Model/class_db.php');
 require_once('./Model/make_db.php');
 
+// Filter input to prevent XSS and SQL Injection
+$vehicle_id = filter_input(INPUT_POST, 'vehicle_id', FILTER_VALIDATE_INT);
+$sort_order = filter_input(INPUT_POST, 'sort_order', FILTER_SANITIZE_STRING);
+$make_id = filter_input(INPUT_POST, 'make_id', FILTER_VALIDATE_INT);
+$type_id = filter_input(INPUT_POST, 'type_id', FILTER_VALIDATE_INT);
+$class_id = filter_input(INPUT_POST, 'class_id', FILTER_VALIDATE_INT);
+
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?: filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?: 'list_assignments';
 switch ($action) {
     default:
